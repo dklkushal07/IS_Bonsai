a027730cc8e6c73be594844a3acb313a8a76ebcc
Kushal Dhakal <68638711+dklkushal07@users.noreply.github.com>
2023-01-20 15:36:47 +0545

    removed console log

--- javascript/nav.js
+++ javascript/nav.js
@@ -30,7 +30,6 @@ window.customElements.define('navigation-bar',NavBar);
 
 let hamburger = document.querySelector(".hamburger");
 let leftMenu = document.querySelector(".nav-bar-left");
-console.log("suvan");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
