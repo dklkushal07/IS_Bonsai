150b9c8b31cf85a890a58cbbe36b51c3639137f3
Aayesha Nakarmi <aayeshanakarmi@gmail.com>
2023-01-22 08:22:20 +0545

    cancel button fixed

--- javascript/about_us.js
+++ javascript/about_us.js
@@ -5,9 +5,22 @@ function popup(){
     modal.classList.toggle('active');
 }
 
-function close(){
+const closeButton = document.getElementById("closeBtn");
+
+closeButton.addEventListener("click", close);
+function close() {
     document.getElementById('about_us').classList.remove('active');
-}
+    document.getElementById('modal').classList.remove('active');
+};
+
+// function close(){
+//     newFunction();
+
+//     function newFunction() {
+//         document.getElementById('about_us').classList.remove('active');
+//         document.getElementById('modal').classList.remove('active');
+//     }
+// }
     // const closeBtn=document.getElementById('closeBtn')
     // closeBtn.addEventListener("click",close(){
     //     this.parentElement.style.display='none';
