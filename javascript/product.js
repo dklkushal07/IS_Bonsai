04e7642ccb26b147cc5a76d790482ae28f0a729f
Suvan Magar <suvanmagar7@gmail.com>
2023-01-29 22:05:50 +0545

    product optimization

---
+++ javascript/product.js
@@ -0,0 +1,103 @@
+function toggle1(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup1 = document.getElementById('popup1');
+    popup1.classList.toggle('active');
+}
+getPrice = function() {
+    var numVal1 = Number(document.getElementById("price").value);
+    var numVal2 = 10 / 100;
+    var totalValue = numVal1 - (numVal1 * numVal2)
+    document.getElementById("total").value = totalValue.toFixed(2);
+}
+function toggle2(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup2 = document.getElementById('popup2');
+    popup2.classList.toggle('active');
+}
+function toggle3(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup3 = document.getElementById('popup3');
+    popup3.classList.toggle('active');
+}
+function toggle4(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup4 = document.getElementById('popup4');
+    popup4.classList.toggle('active');
+}
+function toggle5(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup5 = document.getElementById('popup5');
+    popup5.classList.toggle('active');
+}
+function toggle6(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup6 = document.getElementById('popup6');
+    popup6.classList.toggle('active');
+}
+function toggle7(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup7 = document.getElementById('popup7');
+    popup7.classList.toggle('active');
+}
+function toggle8(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup8 = document.getElementById('popup8');
+    popup8.classList.toggle('active');
+}
+function toggle9(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup9 = document.getElementById('popup9');
+    popup9.classList.toggle('active');
+}
+function toggle10(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup10 = document.getElementById('popup10');
+    popup10.classList.toggle('active');
+}
+function toggle11(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup11 = document.getElementById('popup11');
+    popup11.classList.toggle('active');
+}
+function toggle12(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup12 = document.getElementById('popup12');
+    popup12.classList.toggle('active');
+}
+function toggle13(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup13 = document.getElementById('popup13');
+    popup13.classList.toggle('active');
+}
+function toggle14(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup14 = document.getElementById('popup14');
+    popup14.classList.toggle('active');
+}
+function toggle15(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup15 = document.getElementById('popup15');
+    popup15.classList.toggle('active');
+}
+function toggle16(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup16 = document.getElementById('popup16');
+    popup16.classList.toggle('active');
+}
+
