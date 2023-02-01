808eadf78178585a964ce91a01fa1b0e33adc035
Kushal Dhakal <68638711+dklkushal07@users.noreply.github.com>
2023-02-01 12:26:33 +0545

    completed login/logout

--- javascript/nav.js
+++ javascript/nav.js
@@ -1,8 +1,8 @@
-class NavBar extends HTMLElement{
-    constructor(){
+class NavBar extends HTMLElement {
+    constructor() {
         super();
-        this.innerHTML = 
-        `
+        this.innerHTML =
+            `
         <nav class="nav-bar">
         <a href="home.html" height="60px"><img src="../images/logo with text.png" class="nav-bar-logo"></a>
             <ul class="nav-bar-left">
@@ -13,8 +13,9 @@ class NavBar extends HTMLElement{
                 <li><a href="research.html">Research</a></li>
             </ul>
             <div class="nav-bar-right">
-                <button onclick="cartActivation()"><a src="../images/cart.png">Cart</a></button>
-                <a href="login.html">Login</a>
+            <div id="cart"><button onclick="cartActivation()"><a src="../images/cart.png">Cart</a></button></div>
+                <div class="login-user"><a href="login.html">Login</a></div>
+                <div id="logout"><button onclick="logOut()" ><img src="../images/icons/logout.svg" height="30px"></img></button></div>
                 <div class="hamburger">
                     <span class="bar"></span>
                     <span class="bar"></span>
@@ -42,7 +43,7 @@ class NavBar extends HTMLElement{
     }
 }
 
-window.customElements.define('navigation-bar',NavBar);
+window.customElements.define('navigation-bar', NavBar);
 
 
 let hamburger = document.querySelector(".hamburger");
@@ -53,44 +54,62 @@ hamburger.addEventListener("click", () => {
     leftMenu.classList.toggle("active");
 });
 
+let userNav = document.querySelector(".nav-bar-right");
+let loginStatus = localStorage.getItem("login-status");
+let loginUserToggle = document.querySelector(".login-user");
+let localUsername = (localStorage.getItem("local-username")).substring(1, (localStorage.getItem("local-username")).length - 1);;
+console.log(localUsername)
+if (loginStatus == "yes") {
+    userNav.classList.add('hasUser')
+    loginUserToggle.innerHTML = localUsername;
+}
+
+logOut = () => {
+    if (confirm("Do you really want to sign out?") == true) {
+        localStorage.setItem("login-status","no");
+        userNav.classList.remove('hasUser')
+        loginUserToggle.innerHTML = `<a href="login.html">Login</a>`;
+    }
+}
+
 // cart
 let cartIcon = document.querySelector("#cart");
 let cart = document.querySelector(".cart");
 let closeCart = document.querySelector("#cart-close");
 
-cartActivation = () =>{
+cartActivation = () => {
     cart.classList.add('active');
 };
 
 // CloseCart
-closeCart.onclick = () =>{
+closeCart.onclick = () => {
     cart.classList.remove('active');
 };
 
 // Cart working JS
-if (document.readyState == 'loading'){
+if (document.readyState == 'loading') {
     document.addEventListener('DOMContentLoaded', ready);
-}else {
+} else {
     ready();
 }
 
 // working function
-function ready(){
+function ready() {
     var removeCartButton = document.getElementsByClassName('cart-delete');
     console.log(removeCartButton);
-    for (var i = 0; i < removeCartButton.length; i++){
+    for (var i = 0; i < removeCartButton.length; i++) {
         var button = removeCartButton[i];
         button.addEventListener('click', removeCartItem);
     }
     //Quantity Changes
     var quantityInputs = document.getElementsByClassName('cart-quantity')
-    for (var i = 0; i < quantityInputs.length; i++){
+    for (var i = 0; i < quantityInputs.length; i++) {
         var input = quantityInputs[i];
         input.addEventListener('change', quantityChanged);
     }
     //add to cart
     var addCart = document.getElementsByClassName('add-cart');
-    for (var i = 0; i < addCart.length; i++){
+    for (var i = 0; i < addCart.length; i++) {
         var button = addCart[i];
         button.addEventListener("click", addCartClicked);
     }
@@ -99,33 +118,33 @@ function ready(){
 }
 
 //Buy button
-function butButtonClicked(){
+function butButtonClicked() {
     alert('Your Order is placed')
     var cartContent = document.getElementsByClassName('cart-content')[0];
-    while (cartContent.hasChildNodes()){
+    while (cartContent.hasChildNodes()) {
         cart.textContent.removeChild(cartContent.firstChild);
     }
     updateTotal();
 }
 
 // remove Items from cart
-function removeCartItem(event){
+function removeCartItem(event) {
     var buttonClicked = event.target;
     buttonClicked.parentElement.remove();
     updateTotal();
 }
 
 //Quantity Changes
-function quantityChanged(event){
+function quantityChanged(event) {
     var input = event.target;
-    if(isNaN(input.value) || input.value <= 0){
+    if (isNaN(input.value) || input.value <= 0) {
         input.value = 1
     }
     updateTotal();
 }
 
 //add to cart
-function addCartClicked(event){
+function addCartClicked(event) {
     var button = event.target;
     var shopProducts = button.parentElement;
     var title = shopProducts.getElementsByClassName('product-title')[0].innerText;
@@ -135,15 +154,15 @@ function addCartClicked(event){
     updateTotal();
 }
 
-function addProductToCart(title, price, productImg){
+function addProductToCart(title, price, productImg) {
     var cartShopBox = document.createElement('div');
     cartShopBox.classList.add('cart-box');
     var cartItems = document.getElementsByClassName('cart-content')[0];
     var cartItemNames = document.getElementsByClassName('cart-product-title');
-    for (var i = 0; i < cartItemNames.length; i++){
-        if(cartItemNames[i].innerText == title)
+    for (var i = 0; i < cartItemNames.length; i++) {
+        if (cartItemNames[i].innerText == title)
             alert('You already have this item in your cart');
-            return;
+        return;
     }
 }
 var cartBoxContent = `
@@ -157,30 +176,31 @@ var cartBoxContent = `
 cartShopBox.innerHTML = cartBoxContent;
 cartItems.append(cartShopBox);
 cartShopBox
- .getElementsByClassName('cart-delete')[0]
- .addEventListener("click", removeCartItem);
+    .getElementsByClassName('cart-delete')[0]
+    .addEventListener("click", removeCartItem);
 cartShopBox
- .getElementsByClassName('cart-quantity')[0]
- .addEventListener("change", quantityChanged);
+    .getElementsByClassName('cart-quantity')[0]
+    .addEventListener("change", quantityChanged);
 
 
 
 //Update Total
-function updateTotal(){
+function updateTotal() {
     var cartContent = document.getElementsByClassName('cart-content')[0];
     var cartBoxes = cartContent.getElementsByClassName('cart-box');
     var total = 0;
-    for (var i = 0; i < cartBoxes.length; i++){
+    for (var i = 0; i < cartBoxes.length; i++) {
         var cartBox = cartBoxes[i];
         var priceElement = cartBox.getElementsByClassName('cart-price')[0];
         var quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
         var price = parseFloat(priceElement.innerText.replace("$", ""));
         var quantity = quantityElement.value;
         total = total + (price * quantity);
-        total = Math.round(total * 100)/100;
+        total = Math.round(total * 100) / 100;
     }
 
-        document.getElementsByClassName('total-price')[0].innerText = "$" + total;
+    document.getElementsByClassName('total-price')[0].innerText = "$" + total;
 
-    
+
 }
+
