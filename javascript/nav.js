bf84e7f909d9dc3a49b4f9458b5eec12673f205f
Suvan Magar <suvanmagar7@gmail.com>
2023-02-03 11:12:01 +0545

    suvan finale

--- javascript/nav.js
+++ javascript/nav.js
@@ -32,7 +32,13 @@ class NavBar extends HTMLElement {
                 <h2 class="cart-title">Your cart</h2>
                 <div class="cart-content">
                     <div class="cart-box">
-                    
+                        <img src="../images/1.jpg" class="cart-img">
+                        <div class="detail-box">
+                            <div class="cart-product-title">Sakura</div>
+                            <div class="cart-price">$20</div>
+                            <input type="number" value="1" class="cart-quantity">
+                        </div>
+                        <button class="cart-delete">delete</button>
                     </div>
                 </div>
                 <div class="total">
@@ -124,24 +130,6 @@ function ready() {
         var input = quantityInputs[i];
         input.addEventListener('change', quantityChanged);
     }
-    //add to cart
-    var addCart = document.getElementsByClassName('add-cart');
-    for (var i = 0; i < addCart.length; i++) {
-        var button = addCart[i];
-        button.addEventListener("click", addCartClicked);
-    }
-    //Buy button Work
-    document.getElementsByClassName('btn-buy').addEventListener('click', butButtonClicked)
-}
-
-//Buy button
-function butButtonClicked() {
-    alert('Your Order is placed')
-    var cartContent = document.getElementsByClassName('cart-content')[0];
-    while (cartContent.hasChildNodes()) {
-        cart.textContent.removeChild(cartContent.firstChild);
-    }
-    updateTotal();
 }
 
 // remove Items from cart
@@ -159,48 +147,6 @@ function quantityChanged(event) {
     }
     updateTotal();
 }
-
-//add to cart
-function addCartClicked(event) {
-    var button = event.target;
-    var shopProducts = button.parentElement;
-    var title = shopProducts.getElementsByClassName("product-title")[0].innerText;
-    var price = shopProducts.getElementsByClassName('price')[0].innerText;
-    var productImg = shopProducts.getElementsByClassName('product-image')[0].innerText;
-    console.log(title, price, productImg);
-    // addProductToCart(title, price, productImg);
-    // updateTotal();
-}
-
-function addProductToCart(title, price, productImg) {
-    var cartShopBox = document.createElement('div');
-    cartShopBox.classList.add('cart-box');
-    var cartItems = document.getElementsByClassName('cart-content')[0];
-    var cartItemNames = document.getElementsByClassName('cart-product-title');
-    for (var i = 0; i < cartItemNames.length; i++) {
-        if (cartItemNames[i].innerText == title)
-            alert('You already have this item in your cart');
-        return;
-    }
-}
-var cartBoxContent = `
-                        <img src="${productImg}" class="cart-img">
-                        <div class="detail-box">
-                            <div class="cart-product-title">${title}</div>
-                            <div class="cart-price">${price}</div>
-                            <input type="number" value="1" class="cart-quantity">
-                        </div>
-                        <button class="cart-delete">delete</button>`;
-cartShopBox.innerHTML = cartBoxContent;
-cartItems.append(cartShopBox);
-cartShopBox
-    .getElementsByClassName('cart-delete')[0]
-    .addEventListener("click", removeCartItem);
-cartShopBox
-    .getElementsByClassName('cart-quantity')[0]
-    .addEventListener("change", quantityChanged);
-
-
 
 //Update Total
 function updateTotal() {
--- javascript/product.js
+++ javascript/product.js
@@ -1,103 +1,113 @@
+// toggle1 function to blur the background and bring out the popup component in the DOM
 function toggle1(){
-    var blur = document.getElementById('blur');
+    var blur = document.getElementById('blur');     //brings element with id(blur) and implements the active css in it
     blur.classList.toggle('active');
-    var popup1 = document.getElementById('popup1');
+    var popup1 = document.getElementById('popup1');     //brings element with id(popup1) and implements the active css in it
     popup1.classList.toggle('active');
 }
-getPrice = function() {
-    var numVal1 = Number(document.getElementById("price").value);
-    var numVal2 = 10 / 100;
-    var totalValue = numVal1 - (numVal1 * numVal2)
-    document.getElementById("total").value = totalValue.toFixed(2);
-}
+// toggle2 function to blur the background and bring out the popup component in the DOM
 function toggle2(){
-    var blur = document.getElementById('blur');
+    var blur = document.getElementById('blur');     //brings element with id(blur) and implements the active css in it
     blur.classList.toggle('active');
-    var popup2 = document.getElementById('popup2');
+    var popup2 = document.getElementById('popup2');     //brings element with id(popup2) and implements the active css in it
     popup2.classList.toggle('active');
 }
+// toggle3 function to blur the background and bring out the popup component in the DOM
 function toggle3(){
-    var blur = document.getElementById('blur');
+    var blur = document.getElementById('blur');     //brings element with id(blur) and implements the active css in it
     blur.classList.toggle('active');
-    var popup3 = document.getElementById('popup3');
+    var popup3 = document.getElementById('popup3');     //brings element with id(popup3) and implements the active css in it
     popup3.classList.toggle('active');
 }
+// toggle4 function to blur the background and bring out the popup component in the DOM
 function toggle4(){
-    var blur = document.getElementById('blur');
+    var blur = document.getElementById('blur');     //brings element with id(blur) and implements the active css in it
     blur.classList.toggle('active');
-    var popup4 = document.getElementById('popup4');
+    var popup4 = document.getElementById('popup4');     //brings element with id(popup4) and implements the active css in it
     popup4.classList.toggle('active');
 }
+// toggle5 function to blur the background and bring out the popup component in the DOM
 function toggle5(){
-    var blur = document.getElementById('blur');
+    var blur = document.getElementById('blur');     //brings element with id(blur) and implements the active css in it
     blur.classList.toggle('active');
-    var popup5 = document.getElementById('popup5');
+    var popup5 = document.getElementById('popup5');     //brings element with id(popup5) and implements the active css in it
     popup5.classList.toggle('active');
 }
+// toggle6 function to blur the background and bring out the popup component in the DOM
 function toggle6(){
-    var blur = document.getElementById('blur');
+    var blur = document.getElementById('blur');     //brings element with id(blur) and implements the active css in it
     blur.classList.toggle('active');
-    var popup6 = document.getElementById('popup6');
+    var popup6 = document.getElementById('popup6');     //brings element with id(popup6) and implements the active css in it
     popup6.classList.toggle('active');
 }
+// toggle7 function to blur the background and bring out the popup component in the DOM
 function toggle7(){
-    var blur = document.getElementById('blur');
+    var blur = document.getElementById('blur');     //brings element with id(blur) and implements the active css in it
     blur.classList.toggle('active');
-    var popup7 = document.getElementById('popup7');
+    var popup7 = document.getElementById('popup7');     //brings element with id(popup7) and implements the active css in it
     popup7.classList.toggle('active');
 }
+// toggle8 function to blur the background and bring out the popup component in the DOM
 function toggle8(){
-    var blur = document.getElementById('blur');
+    var blur = document.getElementById('blur');     //brings element with id(blur) and implements the active css in it
     blur.classList.toggle('active');
-    var popup8 = document.getElementById('popup8');
+    var popup8 = document.getElementById('popup8');     //brings element with id(popup8) and implements the active css in it
     popup8.classList.toggle('active');
 }
+// toggle9 function to blur the background and bring out the popup component in the DOM
 function toggle9(){
-    var blur = document.getElementById('blur');
+    var blur = document.getElementById('blur');     //brings element with id(blur) and implements the active css in it
     blur.classList.toggle('active');
-    var popup9 = document.getElementById('popup9');
+    var popup9 = document.getElementById('popup9');     //brings element with id(popup9) and implements the active css in it
     popup9.classList.toggle('active');
 }
+// toggle10 function to blur the background and bring out the popup component in the DOM
 function toggle10(){
-    var blur = document.getElementById('blur');
+    var blur = document.getElementById('blur');     //brings element with id(blur) and implements the active css in it
     blur.classList.toggle('active');
-    var popup10 = document.getElementById('popup10');
+    var popup10 = document.getElementById('popup10');       //brings element with id(popup10) and implements the active css in it
     popup10.classList.toggle('active');
 }
+// toggle11 function to blur the background and bring out the popup component in the DOM
 function toggle11(){
-    var blur = document.getElementById('blur');
+    var blur = document.getElementById('blur');     //brings element with id(blur) and implements the active css in it
     blur.classList.toggle('active');
-    var popup11 = document.getElementById('popup11');
+    var popup11 = document.getElementById('popup11');       //brings element with id(popup11) and implements the active css in it
     popup11.classList.toggle('active');
 }
+// toggle12 function to blur the background and bring out the popup component in the DOM
 function toggle12(){
-    var blur = document.getElementById('blur');
+    var blur = document.getElementById('blur');     //brings element with id(blur) and implements the active css in it
     blur.classList.toggle('active');
-    var popup12 = document.getElementById('popup12');
+    var popup12 = document.getElementById('popup12');       //brings element with id(popup12) and implements the active css in it
     popup12.classList.toggle('active');
 }
+// toggle13 function to blur the background and bring out the popup component in the DOM
 function toggle13(){
-    var blur = document.getElementById('blur');
+    var blur = document.getElementById('blur');     //brings element with id(blur) and implements the active css in it
     blur.classList.toggle('active');
-    var popup13 = document.getElementById('popup13');
+    var popup13 = document.getElementById('popup13');       //brings element with id(popup13) and implements the active css in it
     popup13.classList.toggle('active');
 }
+// toggle14 function to blur the background and bring out the popup component in the DOM
 function toggle14(){
-    var blur = document.getElementById('blur');
+    var blur = document.getElementById('blur');     //brings element with id(blur) and implements the active css in it
     blur.classList.toggle('active');
-    var popup14 = document.getElementById('popup14');
+    var popup14 = document.getElementById('popup14');       //brings element with id(popup14) and implements the active css in it
     popup14.classList.toggle('active');
 }
+// toggle15 function to blur the background and bring out the popup component in the DOM
 function toggle15(){
-    var blur = document.getElementById('blur');
+    var blur = document.getElementById('blur');     //brings element with id(blur) and implements the active css in it
     blur.classList.toggle('active');
-    var popup15 = document.getElementById('popup15');
+    var popup15 = document.getElementById('popup15');       //brings element with id(popup15) and implements the active css in it
     popup15.classList.toggle('active');
 }
+// toggle16 function to blur the background and bring out the popup component in the DOM
 function toggle16(){
-    var blur = document.getElementById('blur');
+    var blur = document.getElementById('blur');     //brings element with id(blur) and implements the active css in it
     blur.classList.toggle('active');
-    var popup16 = document.getElementById('popup16');
+    var popup16 = document.getElementById('popup16');       //brings element with id(popup16) and implements the active css in it
     popup16.classList.toggle('active');
 }
 
